05ee4fd0541fe7ddcaf3d4fc20a696d971cf5505
Varun <barunpandit143@gmail.com>
2023-03-17 12:07:59 +0530

    update

---
+++ product_gallery/curtains_cushions.php
@@ -0,0 +1,5 @@
+<?php include_once('product_header.php'); ?>
+
+
+
+<?php include_once('product_footer.php'); ?>
--- product_gallery/stole.php
+++ product_gallery/stole.php
@@ -1,5 +1,5 @@
-<?php include_once('header.php'); ?>
+<?php include_once('product_header.php'); ?>
 
 
 
-<?php include_once('footer.php'); ?>
+<?php include_once('product_footer.php'); ?>
