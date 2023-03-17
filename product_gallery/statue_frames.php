1774712bf375c7cc36b348c462922edb83935c32
Varun <barunpandit143@gmail.com>
2023-03-17 15:16:14 +0530

    update

--- product_gallery/statue_frames.php
+++ product_gallery/statue_frames.php
@@ -1,5 +1,344 @@
 <?php include_once('product_header.php'); ?>
 
+<!-- Page Header Start -->
+<div class="container-fluid page-header py-5 mb-5">
+    <div class="container py-5">
+        <h1 class="display-3 text-white mb-3 animated slideInDown">Statue & Frames</h1>
+        <nav aria-label="breadcrumb animated slideInDown">
+            <ol class="breadcrumb">
+                <li class="breadcrumb-item"><a class="text-white" href="../index.php">Home</a></li>
+                <li class="breadcrumb-item"><a class="text-white">Product Gallery</a></li>
+                <li class="breadcrumb-item text-white active" aria-current="page">Statue & Frames</li>
+            </ol>
+        </nav>
+    </div>
+</div>
+<!-- Page Header End -->
 
+<!-- Service Start -->
+<div class="container-xxl py-2">
+    <div class="container">
+        <div class="section-title text-center">
+            <h1 class="mb-3">Product Gallery</h1>
+            <h3>Frames</h3>
+            <p>(Product:Dokra)</p>
+        </div>
+        <div class="row g-4">
+            <div class="col-lg-3 col-md-6 portfolio-item wow fadeInUp pt-2" data-wow-delay="0.1s">
+                <div class="rounded overflow-hidden">
+                    <div class="position-relative overflow-hidden">
+                        <img class="img-fluid w-100" src="../img/unicraft/Dokra/dokra-1.jpg" alt="frames">
+                        <div class="portfolio-overlay">
+                            <a class="btn btn-square btn-outline-light mx-1" href="../img/unicraft/Dokra/jute-1.jpg" data-lightbox="portfolio"><i class="fa fa-eye"></i></a>
+                        </div>
+                    </div>
+                    <div class="py-2 text-center">
+                        <button type="button" class="btn btn-primary w-100">Enquiry</button>
+                        <h5 class="lh-base mb-0"></a>
+                    </div>
+                </div>
+            </div>
+            <div class="col-lg-3 col-md-6 portfolio-item wow fadeInUp pt-2" data-wow-delay="0.1s">
+                <div class="rounded overflow-hidden">
+                    <div class="position-relative overflow-hidden">
+                        <img class="img-fluid w-100" src="../img/unicraft/Dokra/dokra-2.jpg" alt="frames">
+                        <div class="portfolio-overlay">
+                            <a class="btn btn-square btn-outline-light mx-1" href="../img/unicraft/Dokra/jute-2.jpg" data-lightbox="portfolio"><i class="fa fa-eye"></i></a>
+                        </div>
+                    </div>
+                    <div class="py-2 text-center">
+                        <button type="button" class="btn btn-primary w-100">Enquiry</button>
+                        <h5 class="lh-base mb-0"></a>
+                    </div>
+                </div>
+            </div>
+            <div class="col-lg-3 col-md-6 portfolio-item wow fadeInUp pt-2" data-wow-delay="0.1s">
+                <div class="rounded overflow-hidden">
+                    <div class="position-relative overflow-hidden">
+                        <img class="img-fluid w-100" src="../img/unicraft/Dokra/dokra-3.jpg" alt="frames">
+                        <div class="portfolio-overlay">
+                            <a class="btn btn-square btn-outline-light mx-1" href="../img/unicraft/Dokra/jute-3.jpg" data-lightbox="portfolio"><i class="fa fa-eye"></i></a>
+                        </div>
+                    </div>
+                    <div class="py-2 text-center">
+                        <button type="button" class="btn btn-primary w-100">Enquiry</button>
+                        <h5 class="lh-base mb-0"></a>
+                    </div>
+                </div>
+            </div>
+            <div class="col-lg-3 col-md-6 portfolio-item wow fadeInUp pt-2" data-wow-delay="0.1s">
+                <div class="rounded overflow-hidden">
+                    <div class="position-relative overflow-hidden">
+                        <img class="img-fluid w-100" src="../img/unicraft/Dokra/dokra-4.jpg" alt="frames">
+                        <div class="portfolio-overlay">
+                            <a class="btn btn-square btn-outline-light mx-1" href="../img/unicraft/Dokra/jute-4.jpg" data-lightbox="portfolio"><i class="fa fa-eye"></i></a>
+                        </div>
+                    </div>
+                    <div class="py-2 text-center">
+                        <button type="button" class="btn btn-primary w-100">Enquiry</button>
+                        <h5 class="lh-base mb-0"></a>
+                    </div>
+                </div>
+            </div>
+            <div class="col-lg-3 col-md-6 portfolio-item wow fadeInUp pt-2" data-wow-delay="0.1s">
+                <div class="rounded overflow-hidden">
+                    <div class="position-relative overflow-hidden">
+                        <img class="img-fluid w-100" src="../img/unicraft/Dokra/dokra-5.jpg" alt="frames">
+                        <div class="portfolio-overlay">
+                            <a class="btn btn-square btn-outline-light mx-1" href="../img/unicraft/Dokra/jute-5.jpg" data-lightbox="portfolio"><i class="fa fa-eye"></i></a>
+                        </div>
+                    </div>
+                    <div class="py-2 text-center">
+                        <button type="button" class="btn btn-primary w-100">Enquiry</button>
+                        <h5 class="lh-base mb-0"></a>
+                    </div>
+                </div>
+            </div>
+
+        </div>
+    </div>
+
+    <div class="container mt-5">
+        <div class="section-title text-center">
+            <h3>Statue</h3>
+            <p>(Product:Dokra)</p>
+        </div>
+        <div class="row g-4">
+            <div class="col-lg-3 col-md-6 portfolio-item wow fadeInUp pt-2" data-wow-delay="0.1s">
+                <div class="rounded overflow-hidden">
+                    <div class="position-relative overflow-hidden">
+                        <img class="img-fluid w-100" src="../img/unicraft/statue/5.jpg" alt="statue">
+                        <div class="portfolio-overlay">
+                            <a class="btn btn-square btn-outline-light mx-1" href="../img/unicraft/statue/5.jpg" data-lightbox="portfolio"><i class="fa fa-eye"></i></a>
+                        </div>
+                    </div>
+                    <div class="py-2 text-center">
+                        <button type="button" class="btn btn-primary w-100">Enquiry</button>
+                        <h5 class="lh-base mb-0"></a>
+                    </div>
+                </div>
+            </div>
+            <div class="col-lg-3 col-md-6 portfolio-item wow fadeInUp pt-2" data-wow-delay="0.1s">
+                <div class="rounded overflow-hidden">
+                    <div class="position-relative overflow-hidden">
+                        <img class="img-fluid w-100" src="../img/unicraft/statue/2.jpg" alt="statue">
+                        <div class="portfolio-overlay">
+                            <a class="btn btn-square btn-outline-light mx-1" href="../img/unicraft/statue/2.jpg" data-lightbox="portfolio"><i class="fa fa-eye"></i></a>
+                        </div>
+                    </div>
+                    <div class="py-2 text-center">
+                        <button type="button" class="btn btn-primary w-100">Enquiry</button>
+                        <h5 class="lh-base mb-0"></a>
+                    </div>
+                </div>
+            </div>
+            <div class="col-lg-3 col-md-6 portfolio-item wow fadeInUp pt-2" data-wow-delay="0.1s">
+                <div class="rounded overflow-hidden">
+                    <div class="position-relative overflow-hidden">
+                        <img class="img-fluid w-100" src="../img/unicraft/statue/3.jpg" alt="statue">
+                        <div class="portfolio-overlay">
+                            <a class="btn btn-square btn-outline-light mx-1" href="../img/unicraft/statue/3.jpg" data-lightbox="portfolio"><i class="fa fa-eye"></i></a>
+                        </div>
+                    </div>
+                    <div class="py-2 text-center">
+                        <button type="button" class="btn btn-primary w-100">Enquiry</button>
+                        <h5 class="lh-base mb-0"></a>
+                    </div>
+                </div>
+            </div>
+            <div class="col-lg-3 col-md-6 portfolio-item wow fadeInUp pt-2" data-wow-delay="0.1s">
+                <div class="rounded overflow-hidden">
+                    <div class="position-relative overflow-hidden">
+                        <img class="img-fluid w-100" src="../img/unicraft/statue/4.jpg" alt="statue">
+                        <div class="portfolio-overlay">
+                            <a class="btn btn-square btn-outline-light mx-1" href="../img/unicraft/statue/4.jpg" data-lightbox="portfolio"><i class="fa fa-eye"></i></a>
+                        </div>
+                    </div>
+                    <div class="py-2 text-center">
+                        <button type="button" class="btn btn-primary w-100">Enquiry</button>
+                        <h5 class="lh-base mb-0"></a>
+                    </div>
+                </div>
+            </div>
+            <div class="col-lg-3 col-md-6 portfolio-item wow fadeInUp pt-2" data-wow-delay="0.1s">
+                <div class="rounded overflow-hidden">
+                    <div class="position-relative overflow-hidden">
+                        <img class="img-fluid w-100" src="../img/unicraft/statue/1.jpg" alt="statue">
+                        <div class="portfolio-overlay">
+                            <a class="btn btn-square btn-outline-light mx-1" href="../img/unicraft/statue/1.jpg" data-lightbox="portfolio"><i class="fa fa-eye"></i></a>
+                        </div>
+                    </div>
+                    <div class="py-2 text-center">
+                        <button type="button" class="btn btn-primary w-100">Enquiry</button>
+                        <h5 class="lh-base mb-0"></a>
+                    </div>
+                </div>
+            </div>
+            <div class="col-lg-3 col-md-6 portfolio-item wow fadeInUp pt-2" data-wow-delay="0.1s">
+                <div class="rounded overflow-hidden">
+                    <div class="position-relative overflow-hidden">
+                        <img class="img-fluid w-100" src="../img/unicraft/statue/6.jpg" alt="statue">
+                        <div class="portfolio-overlay">
+                            <a class="btn btn-square btn-outline-light mx-1" href="../img/unicraft/statue/6.jpg" data-lightbox="portfolio"><i class="fa fa-eye"></i></a>
+                        </div>
+                    </div>
+                    <div class="py-2 text-center">
+                        <button type="button" class="btn btn-primary w-100">Enquiry</button>
+                        <h5 class="lh-base mb-0"></a>
+                    </div>
+                </div>
+            </div>
+            <div class="col-lg-3 col-md-6 portfolio-item wow fadeInUp pt-2" data-wow-delay="0.1s">
+                <div class="rounded overflow-hidden">
+                    <div class="position-relative overflow-hidden">
+                        <img class="img-fluid w-100" src="../img/unicraft/statue/7.jpg" alt="statue">
+                        <div class="portfolio-overlay">
+                            <a class="btn btn-square btn-outline-light mx-1" href="../img/unicraft/statue/7.jpg" data-lightbox="portfolio"><i class="fa fa-eye"></i></a>
+                        </div>
+                    </div>
+                    <div class="py-2 text-center">
+                        <button type="button" class="btn btn-primary w-100">Enquiry</button>
+                        <h5 class="lh-base mb-0"></a>
+                    </div>
+                </div>
+            </div>
+
+        </div>
+    </div>
+
+    <div class="container mt-5">
+        <div class="section-title text-center">
+            <h3>Brass Craft</h3>
+            <p></p>
+        </div>
+        <div class="row g-4">
+            <div class="col-lg-3 col-md-6 portfolio-item wow fadeInUp pt-2" data-wow-delay="0.1s">
+                <div class="rounded overflow-hidden">
+                    <div class="position-relative overflow-hidden">
+                        <img class="img-fluid w-100" src="../img/unicraft/Dokra/dokra-5.jpg" alt="statue">
+                        <div class="portfolio-overlay">
+                            <a class="btn btn-square btn-outline-light mx-1" href="../img/unicraft/Dokra/dokra-5.jpg" data-lightbox="portfolio"><i class="fa fa-eye"></i></a>
+                        </div>
+                    </div>
+                    <div class="py-2 text-center">
+                        <button type="button" class="btn btn-primary w-100">Enquiry</button>
+                        <h5 class="lh-base mb-0"></a>
+                    </div>
+                </div>
+            </div>
+            <div class="col-lg-3 col-md-6 portfolio-item wow fadeInUp pt-2" data-wow-delay="0.1s">
+                <div class="rounded overflow-hidden">
+                    <div class="position-relative overflow-hidden">
+                        <img class="img-fluid w-100" src="../img/unicraft/Dokra/dokra-4.jpg" alt="statue">
+                        <div class="portfolio-overlay">
+                            <a class="btn btn-square btn-outline-light mx-1" href="../img/unicraft/Dokra/dokra-4.jpg" data-lightbox="portfolio"><i class="fa fa-eye"></i></a>
+                        </div>
+                    </div>
+                    <div class="py-2 text-center">
+                        <button type="button" class="btn btn-primary w-100">Enquiry</button>
+                        <h5 class="lh-base mb-0"></a>
+                    </div>
+                </div>
+            </div>
+            <div class="col-lg-3 col-md-6 portfolio-item wow fadeInUp pt-2" data-wow-delay="0.1s">
+                <div class="rounded overflow-hidden">
+                    <div class="position-relative overflow-hidden">
+                        <img class="img-fluid w-100" src="../img/unicraft/Dokra/29(1).jpg" alt="statue">
+                        <div class="portfolio-overlay">
+                            <a class="btn btn-square btn-outline-light mx-1" href="../img/unicraft/Dokra/29(1).jpg" data-lightbox="portfolio"><i class="fa fa-eye"></i></a>
+                        </div>
+                    </div>
+                    <div class="py-2 text-center">
+                        <button type="button" class="btn btn-primary w-100">Enquiry</button>
+                        <h5 class="lh-base mb-0"></a>
+                    </div>
+                </div>
+            </div>
+            <div class="col-lg-3 col-md-6 portfolio-item wow fadeInUp pt-2" data-wow-delay="0.1s">
+                <div class="rounded overflow-hidden">
+                    <div class="position-relative overflow-hidden">
+                        <img class="img-fluid w-100" src="../img/unicraft/Dokra/29(2).jpg" alt="statue">
+                        <div class="portfolio-overlay">
+                            <a class="btn btn-square btn-outline-light mx-1" href="../img/unicraft/Dokra/29(2).jpg" data-lightbox="portfolio"><i class="fa fa-eye"></i></a>
+                        </div>
+                    </div>
+                    <div class="py-2 text-center">
+                        <button type="button" class="btn btn-primary w-100">Enquiry</button>
+                        <h5 class="lh-base mb-0"></a>
+                    </div>
+                </div>
+            </div>
+
+        </div>
+    </div>
+    <div class="container mt-5">
+        <div class="section-title text-center">
+            <h3>Wood Craft</h3>
+            <p></p>
+        </div>
+        <div class="row g-4">
+            <div class="col-lg-3 col-md-6 portfolio-item wow fadeInUp pt-2" data-wow-delay="0.1s">
+                <div class="rounded overflow-hidden">
+                    <div class="position-relative overflow-hidden">
+                        <img class="img-fluid w-100" src="../img/unicraft/Dokra/30(1).jpg" alt="wood craft">
+                        <div class="portfolio-overlay">
+                            <a class="btn btn-square btn-outline-light mx-1" href="../img/unicraft/Dokra/30(1).jpg" data-lightbox="portfolio"><i class="fa fa-eye"></i></a>
+                        </div>
+                    </div>
+                    <div class="py-2 text-center">
+                        <button type="button" class="btn btn-primary w-100">Enquiry</button>
+                        <h5 class="lh-base mb-0"></a>
+                    </div>
+                </div>
+            </div>
+            <div class="col-lg-3 col-md-6 portfolio-item wow fadeInUp pt-2" data-wow-delay="0.1s">
+                <div class="rounded overflow-hidden">
+                    <div class="position-relative overflow-hidden">
+                        <img class="img-fluid w-100" src="../img/unicraft/Dokra/30(2).jpg" alt="wood craft">
+                        <div class="portfolio-overlay">
+                            <a class="btn btn-square btn-outline-light mx-1" href="../img/unicraft/Dokra/30(2).jpg" data-lightbox="portfolio"><i class="fa fa-eye"></i></a>
+                        </div>
+                    </div>
+                    <div class="py-2 text-center">
+                        <button type="button" class="btn btn-primary w-100">Enquiry</button>
+                        <h5 class="lh-base mb-0"></a>
+                    </div>
+                </div>
+            </div>
+            <div class="col-lg-3 col-md-6 portfolio-item wow fadeInUp pt-2" data-wow-delay="0.1s">
+                <div class="rounded overflow-hidden">
+                    <div class="position-relative overflow-hidden">
+                        <img class="img-fluid w-100" src="../img/unicraft/Dokra/30(3).jpg" alt="wood craft">
+                        <div class="portfolio-overlay">
+                            <a class="btn btn-square btn-outline-light mx-1" href="../img/unicraft/Dokra/30(3).jpg" data-lightbox="portfolio"><i class="fa fa-eye"></i></a>
+                        </div>
+                    </div>
+                    <div class="py-2 text-center">
+                        <button type="button" class="btn btn-primary w-100">Enquiry</button>
+                        <h5 class="lh-base mb-0"></a>
+                    </div>
+                </div>
+            </div>
+            <div class="col-lg-3 col-md-6 portfolio-item wow fadeInUp pt-2" data-wow-delay="0.1s">
+                <div class="rounded overflow-hidden">
+                    <div class="position-relative overflow-hidden">
+                        <img class="img-fluid w-100" src="../img/unicraft/Dokra/30(4).jpg" alt="wood craft">
+                        <div class="portfolio-overlay">
+                            <a class="btn btn-square btn-outline-light mx-1" href="../img/unicraft/Dokra/30(4).jpg" data-lightbox="portfolio"><i class="fa fa-eye"></i></a>
+                        </div>
+                    </div>
+                    <div class="py-2 text-center">
+                        <button type="button" class="btn btn-primary w-100">Enquiry</button>
+                        <h5 class="lh-base mb-0"></a>
+                    </div>
+                </div>
+            </div>
+
+        </div>
+    </div>
+
+
+</div>
+<!-- Service End -->
 
 <?php include_once('product_footer.php'); ?>
