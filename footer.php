b78b80036eb266a3733b65dbb470959fec68a8e9
Varun <barunpandit143@gmail.com>
2023-03-17 11:14:03 +0530

    add footer

--- footer.php
+++ footer.php
@@ -4,8 +4,10 @@
         <div class="row g-5">
             <div class="col-lg-3 col-md-6">
                 <h4 class="text-light mb-4">Address</h4>
-                <p class="mb-2"><i class="fa fa-map-marker-alt me-3"></i>123 Street, New York, USA</p>
-                <p class="mb-2"><i class="fa fa-phone-alt me-3"></i>+012 345 67890</p>
+                <p class="mb-2"><i class="fa fa-map-marker-alt me-3"></i>Loomancraft, 1st floor, HP Petrol Pump, Harmu, Ranchi
+                </p>
+                <p class="mb-2"><i class="fa fa-phone-alt me-3"></i>+91 8210196727
+                </p>
                 <p class="mb-2"><i class="fa fa-envelope me-3"></i>info@example.com</p>
                 <div class="d-flex pt-2">
                     <a class="btn btn-outline-light btn-social" href=""><i class="fab fa-twitter"></i></a>
@@ -24,9 +26,9 @@
             </div>
             <div class="col-lg-3 col-md-6">
                 <h4 class="text-light mb-4">Quick Links</h4>
-                <a class="btn btn-link" href="">About Us</a>
-                <a class="btn btn-link" href="">Contact Us</a>
-                <a class="btn btn-link" href="">Our Services</a>
+                <a class="btn btn-link" href="about.php">About Us</a>
+                <a class="btn btn-link" href="contact.php">Contact Us</a>
+                <a class="btn btn-link" href="service.php">Our Services</a>
                 <a class="btn btn-link" href="">Terms & Condition</a>
                 <a class="btn btn-link" href="">Support</a>
             </div>
@@ -44,11 +46,11 @@
         <div class="copyright">
             <div class="row">
                 <div class="col-md-6 text-center text-md-start mb-3 mb-md-0">
-                    &copy; <a class="border-bottom" href="#">Your Site Name</a>, All Right Reserved.
+                    &copy; <a class="border-bottom" href="">2023 Loomang Craft</a>, All Right Reserved.
                 </div>
                 <div class="col-md-6 text-center text-md-end">
-                    <!--/*** This template is free as long as you keep the footer author’s credit link/attribution link/backlink. If you'd like to use the template without the footer author’s credit link/attribution link/backlink, you can purchase the Credit Removal License from "https://htmlcodex.com/credit-removal". Thank you for your support. ***/-->
-                    Designed By <a class="border-bottom" href="https://htmlcodex.com">HTML Codex</a>
+
+                    Designed By <a class="border-bottom" href="https://www.jharkhanditservices.com/" target="_blank">Jharkhand IT Services Software Pvt. Ltd.</a>
                 </div>
             </div>
         </div>
