7c3216e72d9b92ec6f5e88d4140e479e8e7de557
Varun <barunpandit143@gmail.com>
2023-03-17 15:46:59 +0530

    update

--- product_gallery/curtains_cushions.php
+++ product_gallery/curtains_cushions.php
@@ -3,12 +3,12 @@
 <!-- Page Header Start -->
 <div class="container-fluid page-header py-5 mb-5">
     <div class="container py-5">
-        <h1 class="display-3 text-white mb-3 animated slideInDown">Handloom & Leather</h1>
+        <h1 class="display-3 text-white mb-3 animated slideInDown">Curtains & Cushions</h1>
         <nav aria-label="breadcrumb animated slideInDown">
             <ol class="breadcrumb">
                 <li class="breadcrumb-item"><a class="text-white" href="../index.php">Home</a></li>
                 <li class="breadcrumb-item"><a class="text-white">Product Gallery</a></li>
-                <li class="breadcrumb-item text-white active" aria-current="page">Handloom & Leather</li>
+                <li class="breadcrumb-item text-white active" aria-current="page">Curtains & Cushions</li>
             </ol>
         </nav>
     </div>
@@ -20,16 +20,16 @@
     <div class="container">
         <div class="section-title text-center">
             <h1 class="mb-3">Product Gallery</h1>
-            <h3>Handloom & Leather Laptop Bag</h3>
-            <p></p>
+            <h3>Curtains & Cushions</h3>
+            <p>(Product:- curtains and cushions Design:- Tribal Textiles Material :- tussar silk Color: yarn dye)</p>
         </div>
         <div class="row g-4">
             <div class="col-lg-3 col-md-6 portfolio-item wow fadeInUp pt-2" data-wow-delay="0.1s">
                 <div class="rounded overflow-hidden">
                     <div class="position-relative overflow-hidden">
-                        <img class="img-fluid w-100" src="../img/unicraft/handloom/handloom-1.jpg" alt="HANDLOOM">
+                        <img class="img-fluid w-100" src="../img/unicraft/curtains and cushions/cushions-1.jpg" alt="curtains and cushions">
                         <div class="portfolio-overlay">
-                            <a class="btn btn-square btn-outline-light mx-1" href="../img/unicraft/handloom/handloom-1.jpg" data-lightbox="portfolio"><i class="fa fa-eye"></i></a>
+                            <a class="btn btn-square btn-outline-light mx-1" href="../img/unicraft/curtains and cushions/cushions-1.jpg" data-lightbox="portfolio"><i class="fa fa-eye"></i></a>
                         </div>
                     </div>
                     <div class="py-2 text-center">
@@ -41,9 +41,9 @@
             <div class="col-lg-3 col-md-6 portfolio-item wow fadeInUp pt-2" data-wow-delay="0.1s">
                 <div class="rounded overflow-hidden">
                     <div class="position-relative overflow-hidden">
-                        <img class="img-fluid w-100" src="../img/unicraft/handloom/handloom-2.jpg" alt="HANDLOOM">
+                        <img class="img-fluid w-100" src="../img/unicraft/curtains and cushions/cushions-2.jpg" alt="curtains and cushions">
                         <div class="portfolio-overlay">
-                            <a class="btn btn-square btn-outline-light mx-1" href="../img/unicraft/handloom/handloom-2.jpg" data-lightbox="portfolio"><i class="fa fa-eye"></i></a>
+                            <a class="btn btn-square btn-outline-light mx-1" href="../img/unicraft/curtains and cushions/cushions-2.jpg" data-lightbox="portfolio"><i class="fa fa-eye"></i></a>
                         </div>
                     </div>
                     <div class="py-2 text-center">
@@ -55,9 +55,9 @@
             <div class="col-lg-3 col-md-6 portfolio-item wow fadeInUp pt-2" data-wow-delay="0.1s">
                 <div class="rounded overflow-hidden">
                     <div class="position-relative overflow-hidden">
-                        <img class="img-fluid w-100" src="../img/unicraft/handloom/handloom-3.jpg" alt="HANDLOOM">
+                        <img class="img-fluid w-100" src="../img/unicraft/curtains and cushions/cushions-3.jpg" alt="curtains and cushions">
                         <div class="portfolio-overlay">
-                            <a class="btn btn-square btn-outline-light mx-1" href="../img/unicraft/handloom/handloom-3.jpg" data-lightbox="portfolio"><i class="fa fa-eye"></i></a>
+                            <a class="btn btn-square btn-outline-light mx-1" href="../img/unicraft/curtains and cushions/cushions-3.jpg" data-lightbox="portfolio"><i class="fa fa-eye"></i></a>
                         </div>
                     </div>
                     <div class="py-2 text-center">
@@ -69,9 +69,9 @@
             <div class="col-lg-3 col-md-6 portfolio-item wow fadeInUp pt-2" data-wow-delay="0.1s">
                 <div class="rounded overflow-hidden">
                     <div class="position-relative overflow-hidden">
-                        <img class="img-fluid w-100" src="../img/unicraft/handloom/handloom-4.jpg" alt="HANDLOOM">
+                        <img class="img-fluid w-100" src="../img/unicraft/curtains and cushions/cushions-4.jpg" alt="curtains and cushions">
                         <div class="portfolio-overlay">
-                            <a class="btn btn-square btn-outline-light mx-1" href="../img/unicraft/handloom/handloom-4.jpg" data-lightbox="portfolio"><i class="fa fa-eye"></i></a>
+                            <a class="btn btn-square btn-outline-light mx-1" href="../img/unicraft/curtains and cushions/cushions-4.jpg" data-lightbox="portfolio"><i class="fa fa-eye"></i></a>
                         </div>
                     </div>
                     <div class="py-2 text-center">
@@ -81,6 +81,71 @@
                 </div>
             </div>
 
+        </div>
+    </div>
+
+    <div class="container mt-5">
+        <div class="section-title text-center">
+            <h3>Curtains</h3>
+            <p>(Product:- curtains Design:- Handweave Material :- tussar silk Color: yarn dye)</p>
+        </div>
+        <div class="row g-4">
+            <div class="col-lg-3 col-md-6 portfolio-item wow fadeInUp pt-2" data-wow-delay="0.1s">
+                <div class="rounded overflow-hidden">
+                    <div class="position-relative overflow-hidden">
+                        <img class="img-fluid w-100" src="../img/unicraft/Curtains/curtains-1.jpg" alt="Curtains">
+                        <div class="portfolio-overlay">
+                            <a class="btn btn-square btn-outline-light mx-1" href="../img/unicraft/Curtains/curtains-1.jpg" data-lightbox="portfolio"><i class="fa fa-eye"></i></a>
+                        </div>
+                    </div>
+                    <div class="py-2 text-center">
+                        <button type="button" class="btn btn-primary w-100">Enquiry</button>
+                        <h5 class="lh-base mb-0"></a>
+                    </div>
+                </div>
+            </div>
+            <div class="col-lg-3 col-md-6 portfolio-item wow fadeInUp pt-2" data-wow-delay="0.1s">
+                <div class="rounded overflow-hidden">
+                    <div class="position-relative overflow-hidden">
+                        <img class="img-fluid w-100" src="../img/unicraft/Curtains/curtains-2.jpg" alt="Curtains">
+                        <div class="portfolio-overlay">
+                            <a class="btn btn-square btn-outline-light mx-1" href="../img/unicraft/Curtains/curtains-2.jpg" data-lightbox="portfolio"><i class="fa fa-eye"></i></a>
+                        </div>
+                    </div>
+                    <div class="py-2 text-center">
+                        <button type="button" class="btn btn-primary w-100">Enquiry</button>
+                        <h5 class="lh-base mb-0"></a>
+                    </div>
+                </div>
+            </div>
+            <div class="col-lg-3 col-md-6 portfolio-item wow fadeInUp pt-2" data-wow-delay="0.1s">
+                <div class="rounded overflow-hidden">
+                    <div class="position-relative overflow-hidden">
+                        <img class="img-fluid w-100" src="../img/unicraft/Curtains/curtains-3.jpg" alt="Curtains">
+                        <div class="portfolio-overlay">
+                            <a class="btn btn-square btn-outline-light mx-1" href="../img/unicraft/Curtains/curtains-3.jpg" data-lightbox="portfolio"><i class="fa fa-eye"></i></a>
+                        </div>
+                    </div>
+                    <div class="py-2 text-center">
+                        <button type="button" class="btn btn-primary w-100">Enquiry</button>
+                        <h5 class="lh-base mb-0"></a>
+                    </div>
+                </div>
+            </div>
+            <div class="col-lg-3 col-md-6 portfolio-item wow fadeInUp pt-2" data-wow-delay="0.1s">
+                <div class="rounded overflow-hidden">
+                    <div class="position-relative overflow-hidden">
+                        <img class="img-fluid w-100" src="../img/unicraft/Curtains/curtains-4.jpg" alt="Curtains">
+                        <div class="portfolio-overlay">
+                            <a class="btn btn-square btn-outline-light mx-1" href="../img/unicraft/Curtains/curtains-4.jpg" data-lightbox="portfolio"><i class="fa fa-eye"></i></a>
+                        </div>
+                    </div>
+                    <div class="py-2 text-center">
+                        <button type="button" class="btn btn-primary w-100">Enquiry</button>
+                        <h5 class="lh-base mb-0"></a>
+                    </div>
+                </div>
+            </div>
         </div>
     </div>
 
--- product_gallery/shawls_yard.php
+++ product_gallery/shawls_yard.php
@@ -1,5 +1,224 @@
 <?php include_once('product_header.php'); ?>
 
+<!-- Page Header Start -->
+<div class="container-fluid page-header py-5 mb-5">
+    <div class="container py-5">
+        <h1 class="display-3 text-white mb-3 animated slideInDown">Shawls & Gamcha</h1>
+        <nav aria-label="breadcrumb animated slideInDown">
+            <ol class="breadcrumb">
+                <li class="breadcrumb-item"><a class="text-white" href="../index.php">Home</a></li>
+                <li class="breadcrumb-item"><a class="text-white">Product Gallery</a></li>
+                <li class="breadcrumb-item text-white active" aria-current="page">Shawls & Gamcha</li>
+            </ol>
+        </nav>
+    </div>
+</div>
+<!-- Page Header End -->
 
+<!-- Service Start -->
+<div class="container-xxl py-2">
+    <div class="container">
+        <div class="section-title text-center">
+            <h1 class="mb-3">Product Gallery</h1>
+            <h3>Shawls</h3>
+            <p>( Product: Shawls , Width Size:44" , Design:- Handweave, Material: Tussar silk)</p>
+        </div>
+        <div class="row g-4">
+            <div class="col-lg-3 col-md-6 portfolio-item wow fadeInUp pt-2" data-wow-delay="0.1s">
+                <div class="rounded overflow-hidden">
+                    <div class="position-relative overflow-hidden">
+                        <img class="img-fluid w-100" src="../img/unicraft/Shawls/Shawls-1.jpg" alt="Shawls">
+                        <div class="portfolio-overlay">
+                            <a class="btn btn-square btn-outline-light mx-1" href="../img/unicraft/Shawls/Shawls-1.jpg" data-lightbox="portfolio"><i class="fa fa-eye"></i></a>
+                        </div>
+                    </div>
+                    <div class="py-2 text-center">
+                        <button type="button" class="btn btn-primary w-100">Enquiry</button>
+                        <h5 class="lh-base mb-0"></a>
+                    </div>
+                </div>
+            </div>
+            <div class="col-lg-3 col-md-6 portfolio-item wow fadeInUp pt-2" data-wow-delay="0.1s">
+                <div class="rounded overflow-hidden">
+                    <div class="position-relative overflow-hidden">
+                        <img class="img-fluid w-100" src="../img/unicraft/Shawls/Shawls-2.jpg" alt="Shawls">
+                        <div class="portfolio-overlay">
+                            <a class="btn btn-square btn-outline-light mx-1" href="../img/unicraft/Shawls/Shawls-2.jpg" data-lightbox="portfolio"><i class="fa fa-eye"></i></a>
+                        </div>
+                    </div>
+                    <div class="py-2 text-center">
+                        <button type="button" class="btn btn-primary w-100">Enquiry</button>
+                        <h5 class="lh-base mb-0"></a>
+                    </div>
+                </div>
+            </div>
+            <div class="col-lg-3 col-md-6 portfolio-item wow fadeInUp pt-2" data-wow-delay="0.1s">
+                <div class="rounded overflow-hidden">
+                    <div class="position-relative overflow-hidden">
+                        <img class="img-fluid w-100" src="../img/unicraft/Shawls/Shawls-3.jpg" alt="Shawls">
+                        <div class="portfolio-overlay">
+                            <a class="btn btn-square btn-outline-light mx-1" href="../img/unicraft/Shawls/Shawls-3.jpg" data-lightbox="portfolio"><i class="fa fa-eye"></i></a>
+                        </div>
+                    </div>
+                    <div class="py-2 text-center">
+                        <button type="button" class="btn btn-primary w-100">Enquiry</button>
+                        <h5 class="lh-base mb-0"></a>
+                    </div>
+                </div>
+            </div>
+            <div class="col-lg-3 col-md-6 portfolio-item wow fadeInUp pt-2" data-wow-delay="0.1s">
+                <div class="rounded overflow-hidden">
+                    <div class="position-relative overflow-hidden">
+                        <img class="img-fluid w-100" src="../img/unicraft/Shawls/Shawls-4.jpg" alt="Shawls">
+                        <div class="portfolio-overlay">
+                            <a class="btn btn-square btn-outline-light mx-1" href="../img/unicraft/Shawls/Shawls-4.jpg" data-lightbox="portfolio"><i class="fa fa-eye"></i></a>
+                        </div>
+                    </div>
+                    <div class="py-2 text-center">
+                        <button type="button" class="btn btn-primary w-100">Enquiry</button>
+                        <h5 class="lh-base mb-0"></a>
+                    </div>
+                </div>
+            </div>
+            <div class="col-lg-3 col-md-6 portfolio-item wow fadeInUp pt-2" data-wow-delay="0.1s">
+                <div class="rounded overflow-hidden">
+                    <div class="position-relative overflow-hidden">
+                        <img class="img-fluid w-100" src="../img/unicraft/Shawls/Shawls-5.jpg" alt="Shawls">
+                        <div class="portfolio-overlay">
+                            <a class="btn btn-square btn-outline-light mx-1" href="../img/unicraft/Shawls/Shawls-5.jpg" data-lightbox="portfolio"><i class="fa fa-eye"></i></a>
+                        </div>
+                    </div>
+                    <div class="py-2 text-center">
+                        <button type="button" class="btn btn-primary w-100">Enquiry</button>
+                        <h5 class="lh-base mb-0"></a>
+                    </div>
+                </div>
+            </div>
+
+        </div>
+    </div>
+
+    <div class="container mt-5">
+        <div class="section-title text-center">
+            <h3>Santhal Gamcha</h3>
+            <p>( Product: santhanl gamcha , Width Size:36" , Design:- Red Border with white base , Material: khadi cotton)</p>
+        </div>
+        <div class="row g-4 justify-content-center">
+            <div class="col-lg-3 col-md-6 portfolio-item wow fadeInUp pt-2" data-wow-delay="0.1s">
+                <div class="rounded overflow-hidden">
+                    <div class="position-relative overflow-hidden">
+                        <img class="img-fluid w-100" src="../img/unicraft/Shawls/16(3).jpg" alt="Shawls">
+                        <div class="portfolio-overlay">
+                            <a class="btn btn-square btn-outline-light mx-1" href="../img/unicraft/Shawls/16(3).jpg" data-lightbox="portfolio"><i class="fa fa-eye"></i></a>
+                        </div>
+                    </div>
+                    <div class="py-2 text-center">
+                        <button type="button" class="btn btn-primary w-100">Enquiry</button>
+                        <h5 class="lh-base mb-0"></a>
+                    </div>
+                </div>
+            </div>
+
+            <div class="col-lg-3 col-md-6 portfolio-item wow fadeInUp pt-2" data-wow-delay="0.1s">
+                <div class="rounded overflow-hidden">
+                    <div class="position-relative overflow-hidden">
+                        <img class="img-fluid w-100" src="../img/unicraft/Shawls/16(4).jpg" alt="Shawls">
+                        <div class="portfolio-overlay">
+                            <a class="btn btn-square btn-outline-light mx-1" href="../img/unicraft/Shawls/16(4).jpg" data-lightbox="portfolio"><i class="fa fa-eye"></i></a>
+                        </div>
+                    </div>
+                    <div class="py-2 text-center">
+                        <button type="button" class="btn btn-primary w-100">Enquiry</button>
+                        <h5 class="lh-base mb-0"></a>
+                    </div>
+                </div>
+            </div>
+
+
+        </div>
+    </div>
+
+    <div class="container mt-5">
+        <div class="section-title text-center">
+            <h3>Yard(Than)</h3>
+            <p>( Product: yard (Than) , Width Size:44" , Design:- Handweave, Material: silk and cotton, Color: Fabric dye & yarn dye )</p>
+        </div>
+        <div class="row g-4">
+            <div class="col-lg-3 col-md-6 portfolio-item wow fadeInUp pt-2" data-wow-delay="0.1s">
+                <div class="rounded overflow-hidden">
+                    <div class="position-relative overflow-hidden">
+                        <img class="img-fluid w-100" src="../img/unicraft/yard(than)/yard-1.jpg" alt="yard(than)">
+                        <div class="portfolio-overlay">
+                            <a class="btn btn-square btn-outline-light mx-1" href="../img/unicraft/yard(than)/yard-1.jpg" data-lightbox="portfolio"><i class="fa fa-eye"></i></a>
+                        </div>
+                    </div>
+                    <div class="py-2 text-center">
+                        <button type="button" class="btn btn-primary w-100">Enquiry</button>
+                        <h5 class="lh-base mb-0"></a>
+                    </div>
+                </div>
+            </div>
+            <div class="col-lg-3 col-md-6 portfolio-item wow fadeInUp pt-2" data-wow-delay="0.1s">
+                <div class="rounded overflow-hidden">
+                    <div class="position-relative overflow-hidden">
+                        <img class="img-fluid w-100" src="../img/unicraft/yard(than)/yard-2.jpg" alt="yard(than)">
+                        <div class="portfolio-overlay">
+                            <a class="btn btn-square btn-outline-light mx-1" href="../img/unicraft/yard(than)/yard-2.jpg" data-lightbox="portfolio"><i class="fa fa-eye"></i></a>
+                        </div>
+                    </div>
+                    <div class="py-2 text-center">
+                        <button type="button" class="btn btn-primary w-100">Enquiry</button>
+                        <h5 class="lh-base mb-0"></a>
+                    </div>
+                </div>
+            </div>
+            <div class="col-lg-3 col-md-6 portfolio-item wow fadeInUp pt-2" data-wow-delay="0.1s">
+                <div class="rounded overflow-hidden">
+                    <div class="position-relative overflow-hidden">
+                        <img class="img-fluid w-100" src="../img/unicraft/yard(than)/yard-3.jpg" alt="yard(than)">
+                        <div class="portfolio-overlay">
+                            <a class="btn btn-square btn-outline-light mx-1" href="../img/unicraft/yard(than)/yard-3.jpg" data-lightbox="portfolio"><i class="fa fa-eye"></i></a>
+                        </div>
+                    </div>
+                    <div class="py-2 text-center">
+                        <button type="button" class="btn btn-primary w-100">Enquiry</button>
+                        <h5 class="lh-base mb-0"></a>
+                    </div>
+                </div>
+            </div>
+            <div class="col-lg-3 col-md-6 portfolio-item wow fadeInUp pt-2" data-wow-delay="0.1s">
+                <div class="rounded overflow-hidden">
+                    <div class="position-relative overflow-hidden">
+                        <img class="img-fluid w-100" src="../img/unicraft/yard(than)/yard-4.jpg" alt="yard(than)">
+                        <div class="portfolio-overlay">
+                            <a class="btn btn-square btn-outline-light mx-1" href="../img/unicraft/yard(than)/yard-4.jpg" data-lightbox="portfolio"><i class="fa fa-eye"></i></a>
+                        </div>
+                    </div>
+                    <div class="py-2 text-center">
+                        <button type="button" class="btn btn-primary w-100">Enquiry</button>
+                        <h5 class="lh-base mb-0"></a>
+                    </div>
+                </div>
+            </div>
+            <div class="col-lg-3 col-md-6 portfolio-item wow fadeInUp pt-2" data-wow-delay="0.1s">
+                <div class="rounded overflow-hidden">
+                    <div class="position-relative overflow-hidden">
+                        <img class="img-fluid w-100" src="../img/unicraft/yard(than)/yard-5.jpg" alt="yard(than)">
+                        <div class="portfolio-overlay">
+                            <a class="btn btn-square btn-outline-light mx-1" href="../img/unicraft/yard(than)/yard-5.jpg" data-lightbox="portfolio"><i class="fa fa-eye"></i></a>
+                        </div>
+                    </div>
+                    <div class="py-2 text-center">
+                        <button type="button" class="btn btn-primary w-100">Enquiry</button>
+                        <h5 class="lh-base mb-0"></a>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+
+
+</div>
+<!-- Service End -->
 
 <?php include_once('product_footer.php'); ?>
