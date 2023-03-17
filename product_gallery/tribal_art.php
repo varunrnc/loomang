a887cb63f95ab12b282b4980bc10881d8fcd0f6d
Varun <barunpandit143@gmail.com>
2023-03-17 14:43:04 +0530

    update

--- product_gallery/tribal_art.php
+++ product_gallery/tribal_art.php
@@ -1,5 +1,281 @@
 <?php include_once('product_header.php'); ?>
 
+<!-- Page Header Start -->
+<div class="container-fluid page-header py-5 mb-5">
+    <div class="container py-5">
+        <h1 class="display-3 text-white mb-3 animated slideInDown">Tribal Art</h1>
+        <nav aria-label="breadcrumb animated slideInDown">
+            <ol class="breadcrumb">
+                <li class="breadcrumb-item"><a class="text-white" href="../index.php">Home</a></li>
+                <li class="breadcrumb-item"><a class="text-white">Product Gallery</a></li>
+                <li class="breadcrumb-item text-white active" aria-current="page">Tribal Art</li>
+            </ol>
+        </nav>
+    </div>
+</div>
+<!-- Page Header End -->
 
+<!-- Service Start -->
+<div class="container-xxl py-2">
+    <div class="container">
+        <div class="section-title text-center">
+            <h1 class="mb-3">Product Gallery</h1>
+            <h3>Tribal Art</h3>
+            <p>( Product: Tribal Textile , width- customise , Material: silk and cotton, Color:Handpainted and handwoven,Product: stole, scarf, home furnishing,Saree, Dress Material )</p>
+        </div>
+        <div class="row g-4">
+            <div class="col-lg-3 col-md-6 portfolio-item wow fadeInUp pt-2" data-wow-delay="0.1s">
+                <div class="rounded overflow-hidden">
+                    <div class="position-relative overflow-hidden">
+                        <img class="img-fluid w-100" src="../img/unicraft/Tribal textile/textile.jpg" alt="Tribal Art">
+                        <div class="portfolio-overlay">
+                            <a class="btn btn-square btn-outline-light mx-1" href="../img/unicraft/Tribal textile/textile.jpg" data-lightbox="portfolio"><i class="fa fa-eye"></i></a>
+                        </div>
+                    </div>
+                    <div class="py-2 text-center">
+                        <button type="button" class="btn btn-primary w-100">Enquiry</button>
+                        <h5 class="lh-base mb-0"></a>
+                    </div>
+                </div>
+            </div>
+            <div class="col-lg-3 col-md-6 portfolio-item wow fadeInUp pt-2" data-wow-delay="0.1s">
+                <div class="rounded overflow-hidden">
+                    <div class="position-relative overflow-hidden">
+                        <img class="img-fluid w-100" src="../img/unicraft/Tribal textile/textile-2.jpg" alt="Tribal Art">
+                        <div class="portfolio-overlay">
+                            <a class="btn btn-square btn-outline-light mx-1" href="../img/unicraft/Tribal textile/textile-2.jpg" data-lightbox="portfolio"><i class="fa fa-eye"></i></a>
+                        </div>
+                    </div>
+                    <div class="py-2 text-center">
+                        <button type="button" class="btn btn-primary w-100">Enquiry</button>
+                        <h5 class="lh-base mb-0"></a>
+                    </div>
+                </div>
+            </div>
+            <div class="col-lg-3 col-md-6 portfolio-item wow fadeInUp pt-2" data-wow-delay="0.1s">
+                <div class="rounded overflow-hidden">
+                    <div class="position-relative overflow-hidden">
+                        <img class="img-fluid w-100" src="../img/unicraft/Tribal textile/textile-3.jpg" alt="Tribal Art">
+                        <div class="portfolio-overlay">
+                            <a class="btn btn-square btn-outline-light mx-1" href="../img/unicraft/Tribal textile/textile-3.jpg" data-lightbox="portfolio"><i class="fa fa-eye"></i></a>
+                        </div>
+                    </div>
+                    <div class="py-2 text-center">
+                        <button type="button" class="btn btn-primary w-100">Enquiry</button>
+                        <h5 class="lh-base mb-0"></a>
+                    </div>
+                </div>
+            </div>
+            <div class="col-lg-3 col-md-6 portfolio-item wow fadeInUp pt-2" data-wow-delay="0.1s">
+                <div class="rounded overflow-hidden">
+                    <div class="position-relative overflow-hidden">
+                        <img class="img-fluid w-100" src="../img/unicraft/Tribal textile/textile-4.jpg" alt="Tribal Art">
+                        <div class="portfolio-overlay">
+                            <a class="btn btn-square btn-outline-light mx-1" href="../img/unicraft/Tribal textile/textile-4.jpg" data-lightbox="portfolio"><i class="fa fa-eye"></i></a>
+                        </div>
+                    </div>
+                    <div class="py-2 text-center">
+                        <button type="button" class="btn btn-primary w-100">Enquiry</button>
+                        <h5 class="lh-base mb-0"></a>
+                    </div>
+                </div>
+            </div>
+            <div class="col-lg-3 col-md-6 portfolio-item wow fadeInUp pt-2" data-wow-delay="0.1s">
+                <div class="rounded overflow-hidden">
+                    <div class="position-relative overflow-hidden">
+                        <img class="img-fluid w-100" src="../img/unicraft/Tribal textile/textile-5.jpg" alt="Tribal Art">
+                        <div class="portfolio-overlay">
+                            <a class="btn btn-square btn-outline-light mx-1" href="../img/unicraft/Tribal textile/textile-5.jpg" data-lightbox="portfolio"><i class="fa fa-eye"></i></a>
+                        </div>
+                    </div>
+                    <div class="py-2 text-center">
+                        <button type="button" class="btn btn-primary w-100">Enquiry</button>
+                        <h5 class="lh-base mb-0"></a>
+                    </div>
+                </div>
+            </div>
+
+        </div>
+    </div>
+
+    <div class="container mt-5">
+        <div class="section-title text-center">
+            <h3>Tribal Hand Paint</h3>
+            <p>(Product:- Tribal hand paint,Category:- Frames)</p>
+        </div>
+        <div class="row g-4">
+            <div class="col-lg-3 col-md-6 portfolio-item wow fadeInUp pt-2" data-wow-delay="0.1s">
+                <div class="rounded overflow-hidden">
+                    <div class="position-relative overflow-hidden">
+                        <img class="img-fluid w-100" src="../img/unicraft/Tribal hand paint/handpaint-1.jpg" alt="Tribal Hand Paint">
+                        <div class="portfolio-overlay">
+                            <a class="btn btn-square btn-outline-light mx-1" href="../img/unicraft/Tribal hand paint/handpaint-1.jpg" data-lightbox="portfolio"><i class="fa fa-eye"></i></a>
+                        </div>
+                    </div>
+                    <div class="py-2 text-center">
+                        <button type="button" class="btn btn-primary w-100">Enquiry</button>
+                        <h5 class="lh-base mb-0"></a>
+                    </div>
+                </div>
+            </div>
+            <div class="col-lg-3 col-md-6 portfolio-item wow fadeInUp pt-2" data-wow-delay="0.1s">
+                <div class="rounded overflow-hidden">
+                    <div class="position-relative overflow-hidden">
+                        <img class="img-fluid w-100" src="../img/unicraft/Tribal hand paint/handpaint-2.jpg" alt="Tribal Hand Paint">
+                        <div class="portfolio-overlay">
+                            <a class="btn btn-square btn-outline-light mx-1" href="../img/unicraft/Tribal hand paint/handpaint-2.jpg" data-lightbox="portfolio"><i class="fa fa-eye"></i></a>
+                        </div>
+                    </div>
+                    <div class="py-2 text-center">
+                        <button type="button" class="btn btn-primary w-100">Enquiry</button>
+                        <h5 class="lh-base mb-0"></a>
+                    </div>
+                </div>
+            </div>
+            <div class="col-lg-3 col-md-6 portfolio-item wow fadeInUp pt-2" data-wow-delay="0.1s">
+                <div class="rounded overflow-hidden">
+                    <div class="position-relative overflow-hidden">
+                        <img class="img-fluid w-100" src="../img/unicraft/Tribal hand paint/handpaint-3.jpg" alt="Tribal Hand Paint">
+                        <div class="portfolio-overlay">
+                            <a class="btn btn-square btn-outline-light mx-1" href="../img/unicraft/Tribal hand paint/handpaint-3.jpg" data-lightbox="portfolio"><i class="fa fa-eye"></i></a>
+                        </div>
+                    </div>
+                    <div class="py-2 text-center">
+                        <button type="button" class="btn btn-primary w-100">Enquiry</button>
+                        <h5 class="lh-base mb-0"></a>
+                    </div>
+                </div>
+            </div>
+            <div class="col-lg-3 col-md-6 portfolio-item wow fadeInUp pt-2" data-wow-delay="0.1s">
+                <div class="rounded overflow-hidden">
+                    <div class="position-relative overflow-hidden">
+                        <img class="img-fluid w-100" src="../img/unicraft/Tribal hand paint/handpaint-4.jpg" alt="Tribal Hand Paint">
+                        <div class="portfolio-overlay">
+                            <a class="btn btn-square btn-outline-light mx-1" href="../img/unicraft/Tribal hand paint/handpaint-4.jpg" data-lightbox="portfolio"><i class="fa fa-eye"></i></a>
+                        </div>
+                    </div>
+                    <div class="py-2 text-center">
+                        <button type="button" class="btn btn-primary w-100">Enquiry</button>
+                        <h5 class="lh-base mb-0"></a>
+                    </div>
+                </div>
+            </div>
+            <div class="col-lg-3 col-md-6 portfolio-item wow fadeInUp pt-2" data-wow-delay="0.1s">
+                <div class="rounded overflow-hidden">
+                    <div class="position-relative overflow-hidden">
+                        <img class="img-fluid w-100" src="../img/unicraft/Tribal hand paint/handpaint-5.jpg" alt="Tribal Hand Paint">
+                        <div class="portfolio-overlay">
+                            <a class="btn btn-square btn-outline-light mx-1" href="../img/unicraft/Tribal hand paint/handpaint-5.jpg" data-lightbox="portfolio"><i class="fa fa-eye"></i></a>
+                        </div>
+                    </div>
+                    <div class="py-2 text-center">
+                        <button type="button" class="btn btn-primary w-100">Enquiry</button>
+                        <h5 class="lh-base mb-0"></a>
+                    </div>
+                </div>
+            </div>
+            <div class="col-lg-3 col-md-6 portfolio-item wow fadeInUp pt-2" data-wow-delay="0.1s">
+                <div class="rounded overflow-hidden">
+                    <div class="position-relative overflow-hidden">
+                        <img class="img-fluid w-100" src="../img/unicraft/Tribal hand paint/handpaint-6.jpg" alt="Tribal Hand Paint">
+                        <div class="portfolio-overlay">
+                            <a class="btn btn-square btn-outline-light mx-1" href="../img/unicraft/Tribal hand paint/handpaint-6.jpg" data-lightbox="portfolio"><i class="fa fa-eye"></i></a>
+                        </div>
+                    </div>
+                    <div class="py-2 text-center">
+                        <button type="button" class="btn btn-primary w-100">Enquiry</button>
+                        <h5 class="lh-base mb-0"></a>
+                    </div>
+                </div>
+            </div>
+            <div class="col-lg-3 col-md-6 portfolio-item wow fadeInUp pt-2" data-wow-delay="0.1s">
+                <div class="rounded overflow-hidden">
+                    <div class="position-relative overflow-hidden">
+                        <img class="img-fluid w-100" src="../img/unicraft/Tribal hand paint/24(1).jpg" alt="Tribal Hand Paint">
+                        <div class="portfolio-overlay">
+                            <a class="btn btn-square btn-outline-light mx-1" href="../img/unicraft/Tribal hand paint/24(1).jpg" data-lightbox="portfolio"><i class="fa fa-eye"></i></a>
+                        </div>
+                    </div>
+                    <div class="py-2 text-center">
+                        <button type="button" class="btn btn-primary w-100">Enquiry</button>
+                        <h5 class="lh-base mb-0"></a>
+                    </div>
+                </div>
+            </div>
+            <div class="col-lg-3 col-md-6 portfolio-item wow fadeInUp pt-2" data-wow-delay="0.1s">
+                <div class="rounded overflow-hidden">
+                    <div class="position-relative overflow-hidden">
+                        <img class="img-fluid w-100" src="../img/unicraft/Tribal hand paint/24(2).jpg" alt="Tribal Hand Paint">
+                        <div class="portfolio-overlay">
+                            <a class="btn btn-square btn-outline-light mx-1" href="../img/unicraft/Tribal hand paint/24(2).jpg" data-lightbox="portfolio"><i class="fa fa-eye"></i></a>
+                        </div>
+                    </div>
+                    <div class="py-2 text-center">
+                        <button type="button" class="btn btn-primary w-100">Enquiry</button>
+                        <h5 class="lh-base mb-0"></a>
+                    </div>
+                </div>
+            </div>
+            <div class="col-lg-3 col-md-6 portfolio-item wow fadeInUp pt-2" data-wow-delay="0.1s">
+                <div class="rounded overflow-hidden">
+                    <div class="position-relative overflow-hidden">
+                        <img class="img-fluid w-100" src="../img/unicraft/Tribal hand paint/24(3).jpg" alt="Tribal Hand Paint">
+                        <div class="portfolio-overlay">
+                            <a class="btn btn-square btn-outline-light mx-1" href="../img/unicraft/Tribal hand paint/24(3).jpg" data-lightbox="portfolio"><i class="fa fa-eye"></i></a>
+                        </div>
+                    </div>
+                    <div class="py-2 text-center">
+                        <button type="button" class="btn btn-primary w-100">Enquiry</button>
+                        <h5 class="lh-base mb-0"></a>
+                    </div>
+                </div>
+            </div>
+            <div class="col-lg-3 col-md-6 portfolio-item wow fadeInUp pt-2" data-wow-delay="0.1s">
+                <div class="rounded overflow-hidden">
+                    <div class="position-relative overflow-hidden">
+                        <img class="img-fluid w-100" src="../img/unicraft/Tribal hand paint/24(4).jpg" alt="Tribal Hand Paint">
+                        <div class="portfolio-overlay">
+                            <a class="btn btn-square btn-outline-light mx-1" href="../img/unicraft/Tribal hand paint/24(4).jpg" data-lightbox="portfolio"><i class="fa fa-eye"></i></a>
+                        </div>
+                    </div>
+                    <div class="py-2 text-center">
+                        <button type="button" class="btn btn-primary w-100">Enquiry</button>
+                        <h5 class="lh-base mb-0"></a>
+                    </div>
+                </div>
+            </div>
+            <div class="col-lg-3 col-md-6 portfolio-item wow fadeInUp pt-2" data-wow-delay="0.1s">
+                <div class="rounded overflow-hidden">
+                    <div class="position-relative overflow-hidden">
+                        <img class="img-fluid w-100" src="../img/unicraft/Tribal hand paint/24(5).jpg" alt="Tribal Hand Paint">
+                        <div class="portfolio-overlay">
+                            <a class="btn btn-square btn-outline-light mx-1" href="../img/unicraft/Tribal hand paint/24(5).jpg" data-lightbox="portfolio"><i class="fa fa-eye"></i></a>
+                        </div>
+                    </div>
+                    <div class="py-2 text-center">
+                        <button type="button" class="btn btn-primary w-100">Enquiry</button>
+                        <h5 class="lh-base mb-0"></a>
+                    </div>
+                </div>
+            </div>
+            <div class="col-lg-3 col-md-6 portfolio-item wow fadeInUp pt-2" data-wow-delay="0.1s">
+                <div class="rounded overflow-hidden">
+                    <div class="position-relative overflow-hidden">
+                        <img class="img-fluid w-100" src="../img/unicraft/Tribal hand paint/24(6).jpg" alt="Tribal Hand Paint">
+                        <div class="portfolio-overlay">
+                            <a class="btn btn-square btn-outline-light mx-1" href="../img/unicraft/Tribal hand paint/24(6).jpg" data-lightbox="portfolio"><i class="fa fa-eye"></i></a>
+                        </div>
+                    </div>
+                    <div class="py-2 text-center">
+                        <button type="button" class="btn btn-primary w-100">Enquiry</button>
+                        <h5 class="lh-base mb-0"></a>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+
+</div>
+<!-- Service End -->
 
 <?php include_once('product_footer.php'); ?>
