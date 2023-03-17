92f206c3db01ccef437535767773313b2fef7873
Varun <barunpandit143@gmail.com>
2023-03-17 15:59:36 +0530

    update

--- product_gallery/our_team.php
+++ product_gallery/our_team.php
@@ -1,5 +1,117 @@
 <?php include_once('product_header.php'); ?>
 
+<!-- Page Header Start -->
+<div class="container-fluid page-header py-5 mb-5">
+    <div class="container py-5">
+        <h1 class="display-3 text-white mb-3 animated slideInDown">Our Team</h1>
+        <nav aria-label="breadcrumb animated slideInDown">
+            <ol class="breadcrumb">
+                <li class="breadcrumb-item"><a class="text-white" href="../index.php">Home</a></li>
+                <li class="breadcrumb-item"><a class="text-white">Product Gallery</a></li>
+                <li class="breadcrumb-item text-white active" aria-current="page">Our Team</li>
+            </ol>
+        </nav>
+    </div>
+</div>
+<!-- Page Header End -->
 
+<!-- Service Start -->
+<div class="container-xxl py-2">
+    <div class="container">
+        <div class="section-title text-center">
+            <h1 class="mb-3">Our Team</h1>
+            <h3></h3>
+            <p></p>
+        </div>
+        <div class="row g-4">
+            <div class="col-lg-3 col-md-6 portfolio-item wow fadeInUp pt-2" data-wow-delay="0.1s">
+                <div class="rounded overflow-hidden">
+                    <div class="position-relative overflow-hidden">
+                        <img class="img-fluid w-100" src="../img/unicraft/team/35(1).jpg" alt="team">
+                        <div class="portfolio-overlay">
+                            <a class="btn btn-square btn-outline-light mx-1" href="../img/unicraft/team/35(1).jpg" data-lightbox="portfolio"><i class="fa fa-eye"></i></a>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <div class="col-lg-3 col-md-6 portfolio-item wow fadeInUp pt-2" data-wow-delay="0.1s">
+                <div class="rounded overflow-hidden">
+                    <div class="position-relative overflow-hidden">
+                        <img class="img-fluid w-100" src="../img/unicraft/team/35(2).jpg" alt="team">
+                        <div class="portfolio-overlay">
+                            <a class="btn btn-square btn-outline-light mx-1" href="../img/unicraft/team/35(2).jpg" data-lightbox="portfolio"><i class="fa fa-eye"></i></a>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <div class="col-lg-3 col-md-6 portfolio-item wow fadeInUp pt-2" data-wow-delay="0.1s">
+                <div class="rounded overflow-hidden">
+                    <div class="position-relative overflow-hidden">
+                        <img class="img-fluid w-100" src="../img/unicraft/team/35(3).jpg" alt="team">
+                        <div class="portfolio-overlay">
+                            <a class="btn btn-square btn-outline-light mx-1" href="../img/unicraft/team/35(3).jpg" data-lightbox="portfolio"><i class="fa fa-eye"></i></a>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <div class="col-lg-3 col-md-6 portfolio-item wow fadeInUp pt-2" data-wow-delay="0.1s">
+                <div class="rounded overflow-hidden">
+                    <div class="position-relative overflow-hidden">
+                        <img class="img-fluid w-100" src="../img/unicraft/team/35(4).jpg" alt="team">
+                        <div class="portfolio-overlay">
+                            <a class="btn btn-square btn-outline-light mx-1" href="../img/unicraft/team/35(4).jpg" data-lightbox="portfolio"><i class="fa fa-eye"></i></a>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <div class="col-lg-3 col-md-6 portfolio-item wow fadeInUp pt-2" data-wow-delay="0.1s">
+                <div class="rounded overflow-hidden">
+                    <div class="position-relative overflow-hidden">
+                        <img class="img-fluid w-100" src="../img/unicraft/team/35(5).jpg" alt="team">
+                        <div class="portfolio-overlay">
+                            <a class="btn btn-square btn-outline-light mx-1" href="../img/unicraft/team/35(5).jpg" data-lightbox="portfolio"><i class="fa fa-eye"></i></a>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <div class="col-lg-3 col-md-6 portfolio-item wow fadeInUp pt-2" data-wow-delay="0.1s">
+                <div class="rounded overflow-hidden">
+                    <div class="position-relative overflow-hidden">
+                        <img class="img-fluid w-100" src="../img/unicraft/team/35(6).jpg" alt="team">
+                        <div class="portfolio-overlay">
+                            <a class="btn btn-square btn-outline-light mx-1" href="../img/unicraft/team/35(6).jpg" data-lightbox="portfolio"><i class="fa fa-eye"></i></a>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <div class="col-lg-3 col-md-6 portfolio-item wow fadeInUp pt-2" data-wow-delay="0.1s">
+                <div class="rounded overflow-hidden">
+                    <div class="position-relative overflow-hidden">
+                        <img class="img-fluid w-100" src="../img/unicraft/team/35(7).jpg" alt="team">
+                        <div class="portfolio-overlay">
+                            <a class="btn btn-square btn-outline-light mx-1" href="../img/unicraft/team/35(7).jpg" data-lightbox="portfolio"><i class="fa fa-eye"></i></a>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <div class="col-lg-3 col-md-6 portfolio-item wow fadeInUp pt-2" data-wow-delay="0.1s">
+                <div class="rounded overflow-hidden">
+                    <div class="position-relative overflow-hidden">
+                        <img class="img-fluid w-100" src="../img/unicraft/team/35(8).jpg" alt="team">
+                        <div class="portfolio-overlay">
+                            <a class="btn btn-square btn-outline-light mx-1" href="../img/unicraft/team/35(8).jpg" data-lightbox="portfolio"><i class="fa fa-eye"></i></a>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+
+
+
+
+
+</div>
+<!-- Service End -->
 
 <?php include_once('product_footer.php'); ?>
