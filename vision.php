fa2b250c08d3f8fc6b058b131daf093240c6c306
NikhilSingh97 <niki97krsingh@gmail.com>
2023-03-17 16:24:03 +0530

    update

--- vision.php
+++ vision.php
@@ -2,7 +2,7 @@
 
 
 <!-- Page Header Start -->
-<div class="container-fluid page-header py-5 mb-5">
+<div class="container-fluid page-header py-5 mb-3">
     <div class="container py-5">
         <h1 class="display-3 text-white mb-3 animated slideInDown">Vision</h1>
         <nav aria-label="breadcrumb animated slideInDown">
@@ -14,22 +14,23 @@
     </div>
 </div>
 <!-- Page Header End -->
-
+<div class="container-fluid">
+    <div class="section-title  text-center">
+        <h1 class="display-5 mb-4 ">Vision</h1>
+    </div>
+</div>
 
 <!-- Vision Start -->
-<div class="container-fluid bg-light overflow-hidden px-lg-0">
-    <div class="container about px-lg-0">
+<div class="container-fluid bg-light overflow-hidden px-lg-0 ">
+    <div class="container about px-lg-0 my-4">
         <div class="row g-0 mx-lg-0 justify-content-center">
-            <div class="col-lg-4 ps-lg-0 pt-5" style="min-height: 400px;">
+            <div class="col-lg-4 ps-lg-0   vision">
                 <div class="position-relative  ">
-                    <img class="position-absolute img-fluid px-3" src="img/prdcts/vision.jpg" style="object-fit: cover;" alt="">
+                    <img class="position-absolute img-fluid px-3 " src="img/prdcts/vision.jpg" style="object-fit: cover;" alt="">
                 </div>
             </div>
             <div class="col-lg-6 pt-3 about-text wow fadeIn" data-wow-delay="0.5s">
                 <div class=" pe-lg-0">
-                    <div class="section-title text-start text-center">
-                        <h1 class="display-5 mb-4 ">Vision</h1>
-                    </div>
                     <p class=" ps-4" align="justify">We are Jharkhand based start-up, established in June 2018(MSME’s Reg.)
                         working towards conservation, development and promotion of the indigenous and traditional
                         arts and crafts forms of the state, and thereby bring positive change in the lives of the
