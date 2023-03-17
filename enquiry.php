c2608572a19e29f75c20b6003905f3fd98b21922
NikhilSingh97 <niki97krsingh@gmail.com>
2023-03-17 16:42:56 +0530

    changse

--- enquiry.php
+++ enquiry.php
@@ -16,13 +16,157 @@
 </div>
 <!-- Page Header End -->
 <!-- ======= Booking Section ======= -->
-<section id="booking" class="enquiry mt-2 " style="background-color: #ffffff;">
+<div class="container-fluid bg-light overflow-hidden px-lg-5" style="margin: 1rem 0;">
+    <div class="container contact px-lg-0">
+        <div class="row g-0 mx-lg-0 justify-content-center">
+            <div class="col-lg-9 contact-text  wow fadeIn" data-wow-delay="0.5s">
+                <div class="p-lg-5 ps-lg-0">
+                    <div class="section-title text-center">
+                        <h1 class="display-5 mb-4">Contact Us</h1>
+                    </div>
+                    <form action="#" method="POST">
+                        <div class="row">
+                            <div class="col-md-12">
+                                <div class="mb-3">
+                                    <label class="form-label"> <b>Select a Product</b> </label> <br>
+                                    <select name="product" class="form-select" aria-label="Default select example">
+
+                                        <option value="Stole:(Size:- 24”/90”   Design:- Handpainted   Material :- Tasar* Eri )">
+                                            Stole:(Size:- 24”/90” Design:- Handpainted Material :- Tasar* Eri )</option>
+                                        <option value="Stole:(Size:- 24”/90”   Design:- Hand Block Painted   Material :- Tasar* Eri )">
+                                            Stole:(Size:- 24”/90” Design:- Hand Block Painted Material :- Tasar* Eri )
+                                        </option>
+                                        <option value="Stole:(Size:- 24”/90”   Design:- Hand embroidered   Material :- Tasar* Eri )">
+                                            Stole:(Size:- 24”/90” Design:- Hand embroidered Material :- Tasar* Eri )
+                                        </option>
+                                        <option value="(Size:- 24”/90”   Design:- Hand embroidered(hand quilted)   Material :- silk )">
+                                            Stole:(Size:- 24”/90” Design:- Hand embroidered(hand quilted) Material :-
+                                            silk )</option>
+                                        <option value="Stole:(Size:- 24”/90”   Design:- Hand Painted(Madhubani)   Material :- silk )">
+                                            Stole:(Size:- 24”/90” Design:- Hand Painted(Madhubani) Material :- silk )
+                                        </option>
+                                        <option value="Stole:(Size:- 24”/90”   Design:- Hand Painted(Sohrai,Jadopatia)   Material :- silk )">
+                                            Stole:(Size:- 24”/90” Design:- Hand Painted(Sohrai,Jadopatia) Material :-
+                                            silk )</option>
+                                        <option value="Men Tie:( Product: Men Tie , Size:75, Design:- Handweave, Material: Handspun Tasar )">
+                                            Men Tie:( Product: Men Tie , Size:75" , Design:- Handweave, Material:
+                                            Handspun Tasar )</option>
+                                        <option value="Paintings & Frames:(Product:- Painting Nature,BIGFRAMES)">
+                                            Paintings & Frames:(Product:- Painting Nature,BIGFRAMES)</option>
+                                        <option value="Paintings & Frames:(Product:- Tribal Couple Dancing,BIGFRAMES)">
+                                            Paintings & Frames:(Product:- Tribal Couple Dancing,BIGFRAMES)</option>
+                                        <option value="Paintings & Frames:(Product:-Madhubani,Painting Nature,BIGFRAMES)">
+                                            Paintings & Frames:(Product:-Madhubani,Painting Nature,BIGFRAMES)</option>
+                                        <option value="Tribal textile:( Material: silk and cotton, Color:Handpainted and handwoven,Product: stole, scarf, home furnishing,Saree, Dress Material )">
+                                            Tribal textile:( Material: silk and cotton, Color:Handpainted and
+                                            handwoven,Product: stole, scarf, home furnishing,Saree, Dress Material )
+                                        </option>
+                                        <option value="Tribal hand paint:(Product:- Tribal hand paint,Category:- Frames)">
+                                            Tribal hand paint:(Product:- Tribal hand paint,Category:- Frames)"</option>
+                                        <option value="Jute Product:(Category:- File Folder, kit Bag, Laptop Bag )">
+                                            Jute Product:(Category:- File Folder, kit Bag, Laptop Bag )</option>
+                                        <option value="Jute Carry Bag">
+                                            Jute Carry Bag</option>
+                                        <option value="Jute Carry Bag">
+                                            Handmade Dairy</option>
+                                        <option value="Frames:(Product:Dokra)">
+                                            Frames:(Product:Dokra)</option>
+                                        <option value="Statue:(Product:Dokra)">
+                                            Statue:(Product:Dokra)</option>
+                                        <option value="Brass Craft">
+                                            Brass Craft</option>
+                                        <option value="Brass Craft">
+                                            Wood Craft</option>
+                                        <option value="Handloom & Leather Laptop Bag">
+                                            Handloom & Leather Laptop Bag</option>
+                                        <option value="Curtains & Cushions:(Product:- curtains and cushions Design:- Tribal Textiles Material :- tussar silk Color: yarn dye)">
+                                            Curtains & Cushions:(Product:- curtains and cushions Design:- Tribal
+                                            Textiles Material :- tussar silk Color: yarn dye)</option>
+                                        <option value="Curtains:(Product:- curtains Design:- Handweave Material :- tussar silk Color: yarn dye)">
+                                            Curtains:(Product:- curtains Design:- Handweave Material :- tussar silk
+                                            Color: yarn dye)</option>
+                                        <option value="Necklace:(Product:- Necklace, Design:- Dokra(Brass), Material :- Metal with cotton thread )">
+                                            Necklace:(Product:- Necklace, Design:- Dokra(Brass), Material :- Metal with
+                                            cotton thread )</option>
+                                        <option value="Tribal Jewellery:(Product:- Tribal Jewellery , Design:- Traditional Tribal Based , Material :- Metal Casted such as dokra, white metal, Terracotta, lac etc.)">
+                                            Tribal Jewellery:(Product:- Tribal Jewellery , Design:- Traditional Tribal
+                                            Based , Material :- Metal Casted such as dokra, white metal, Terracotta, lac
+                                            etc.)</option>
+                                        <option value="Shawls:( Product: Shawls , Width Size:44' , Design:- Handweave, Material: Tussar silk)">
+                                            Shawls:( Product: Shawls , Width Size:44' , Design:- Handweave, Material:
+                                            Tussar silk)</option>
+                                        <option value="
+                            Santhal Gamcha:( Product: santhanl gamcha , Width Size:36' , Design:- Red Border with white base , Material: khadi cotton)">
+                                            Santhal Gamcha:( Product: santhanl gamcha , Width Size:36' , Design:- Red
+                                            Border with white base , Material: khadi cotton)</option>
+                                        <option value="
+                            Yard(Than):( Product: yard (Than) , Width Size:44' , Design:- Handweave, Material: silk and cotton, Color: Fabric dye & yarn dye )">
+                                            Yard(Than):( Product: yard (Than) , Width Size:44' , Design:- Handweave,
+                                            Material: silk and cotton, Color: Fabric dye & yarn dye )</option>
+                                    </select>
+
+
+                                </div>
+                            </div>
+                            <div class="col-md-6">
+                                <div class="mb-3">
+                                    <label class="form-label"> <b>Name</b> </label>
+                                    <input type="text" name="name" class="form-control" required>
+                                </div>
+                            </div>
+                            <div class="col-md-6">
+                                <div class="mb-3">
+                                    <label class="form-label"> <b>Email</b> </label>
+                                    <input type="email" name="email" class="form-control">
+                                </div>
+                            </div>
+                            <div class="col-md-6">
+                                <div class="mb-3">
+                                    <label class="form-label"> <b>Gender</b> </label>
+                                    <select name="gender" class="form-select" aria-label="Default select example">
+
+                                        <option value="Male">Male</option>
+                                        <option value="Female">Female</option>
+                                        <option value="Others">Others</option>
+                                    </select>
+                                </div>
+                            </div>
+
+                            <div class="col-md-6">
+                                <div class="mb-3">
+                                    <label class="form-label"> <b>Contact Number</b> </label>
+                                    <input type="text" name="phone" class="form-control">
+                                </div>
+                            </div>
+                            <div class="col-md-12">
+                                <div class="mb-3">
+                                    <label class="form-label"> <b>Enquiry</b> </label>
+                                    <textarea class="form-control" name="enquiry" rows="3"></textarea>
+
+                                </div>
+                            </div>
+
+                            <div class="col-md-12 text-center">
+                                <button type="submit" class="btn btn-primary  btn-lg fs-5 fw-semibold px-5 mt-2 mb-3 mb-5" style="">Enquiry
+                                    Now</button>
+                            </div>
+
+                        </div>
+
+                    </form>
+                </div>
+            </div>
+
+        </div>
+    </div>
+</div>
+<!-- <section id="booking" class="enquiry mt-2 " style="background-color: #ffffff;">
     <div class="container">
         <div class="card-body">
             <div class="row justify-content-center content">
                 <div class="col-lg-8 pt-4 pt-lg-0">
-                    <div class="section-title mb-2">
-                        <h2 class="text-center">Enquiry Page</h2>
+                    <div class="section-title text-center">
+                        <h1 class="display-5 mb-4">Enquiry Page</h1>
                     </div>
                     <form action="#" method="POST">
 
@@ -147,16 +291,7 @@
 
                                 </div>
                             </div>
-                            <div class="col-md-12">
-                                <div class="mb-3">
-                                    <div class="row justify-content-center">
-                                        <div class="col-md-8 text-center" id="loading">
-                                            <img style="width: 40px; height:40px;" src="public/assets/img/load.jpg" />
-                                        </div>
-                                    </div>
-
-                                </div>
-                            </div>
+                           
                             <div class="col-md-12 text-center">
                                 <button type="submit" class="btn btn-primary  btn-lg fs-5 fw-semibold px-5 mt-2 mb-3 mb-5" style="">Enquiry
                                     Now</button>
@@ -171,6 +306,8 @@
         </div>
 
     </div>
-</section><!-- End Booking Section -->
+</section> -->
+
+<!-- End Booking Section -->
 
 <?php include_once('footer.php'); ?>
